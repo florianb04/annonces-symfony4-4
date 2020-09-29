de13e005bca70bcafd0dca9880eb9ae4389f3259
FlorianB04 <florianb04@gmail.com>
2020-09-29 13:06:54 +0200

    add new syntax to learn in for test

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -15,13 +15,17 @@ class TestController extends AbstractController
      */
     public function index()
     {
+        // tableau
         $tab = ['Eric'=>52,'Gerald'=>53,'Florian'=>26];
 
+        // variable simple
         $a = 'Wael';
 
         dump($tab);
         dump($a);
 
+        // on associe les varible PHP en valeur avec des clefs twig
+        // on pourra utiliser les varible twig 
         return $this->render('test/test.html.twig', [
             'tableau'=>$tab,
             'prenom'=>$a,
