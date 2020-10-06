b3acd58e53d4e8f445388514049dfef1a387876b
FlorianB04 <florianb04@gmail.com>
2020-10-06 12:19:07 +0200

    fill in the fixures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,55 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Ad;
+use Cocur\Slugify\Slugify;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
-        $manager->flush();
+
+
+        for ($i=0; $i < 30; $i++) { 
+
+            // step by step slugigy use
+            $slugify = new Slugify();
+            $title = "Titre de l'annonce n : $i";
+            $slug = $slugify->slugify($title);
+            
+            $ad = new Ad ();
+            $ad->setTitle("Titre de l'annonce n : $i ")
+            ->setSlug($slug)
+            ->setPrice(mt_rand(40,200))
+            ->setIntroduction("<p> introduction de l'annonce n : $i </p>")
+            ->setContent("<p>  contenu de l'annonce n : $i </p>")
+            ->setRooms(mt_rand(1,5)) 
+            ->setCoverImage("https://via.placeholder.com/350");
+        
+            // keep in temp memory to don't erase for every turn "for"
+            $manager->persist($ad);
+
+            // write in the DB
+            $manager->flush();
+
+
+            // Concatenation of the old slug with the ID to be sure the the slug is unique
+            $slug2 = $ad->getSlug().'_'.$ad->getId();
+            $ad->setSlug($slug2);
+
+            $manager->persist($ad);
+            $manager->flush();
+
+
+            //dump($ad->getId());
+
+        }
+
+        // $manager->flush();
+
     }
+
 }
